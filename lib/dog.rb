1d93231ef41fc2a697244a657b29f7f58e46b6fc
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-23 23:50:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
 class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+  def initialize(name)
+    @name = name
   end
  
   def name
-    this_dogs_name
+    @name
   end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
 end
